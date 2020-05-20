7d7160087ccdf0a27e2e621bc1516fb370da4323
Adam Wolff <adamkwolff@gmail.com>
2020-05-20 03:24:05 -0500

    finally transfered my files to the correct repo. about to make some changes to css

--- assets/Javascript/apps.js
+++ assets/Javascript/apps.js
@@ -11,22 +11,110 @@
 //button "dogs" are added (push to the array of topics)
 //there is a submit button to the form
 //check with bootstrap to add this form -- vertical menu on the right
-$(document).ready() {
-    var key = "teJTuCyxCldAJlcKZn3VFRwd9sf00UsT";
-    var topics = [
-    ]
-    var url = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
-    topics.done(function (data) { console.log("success got data", data); });
-    $(".gif").on("click", function () {
-        var state = $(this).data("state");
-        var stillImage = $(this).data("still");
-        var animatedImage = $(this).data("animate");
-        if (state === "still") {
-            $(this).data("state", "animate");
-            $(this).attr("src", animatedImage);
-        } else {
-            $(this).data("state", "still");
-            $(this).attr("src", stillImage);
-        }
+$(document).ready()
+//topics array
+var topics = ["cats", "horses", "pigs", "Paris", "asian food", "James Cordon", "007", "exercise",
+  "dog walking", "boxer dog", "bullmastiff", "Hawaii", "The Office", "Andy Griffith", "Hawaii five-O",
+  "Magnum PI", "Outlander", "hawaiian food", "hula", "Star Trek", "Star Wars", "Grand Canyon",
+  "Colorado", "Lipsync Battle", "Forrest Gump", "clydesdales", "sleeping dogs", "giraffes"];
+//api key
+var key = "r8vfoFb9SvSLxNCssr7kk6NnXjK4RuZf";
+//still and animate
+function animateThis() {
+  var state = $(this).attr("data-state");
+  //if image is still
+  if (state === "still") {
+    //setting url - switch
+    $(this).attr("src", $(this).attr("data-animate"));
+    //requesting animate
+    $(this).attr("data-state", "animate");
+  } else {
+    //switch to still
+    $(this).attr("src", $(this).attr("data-still"));
+    //image state
+    $(this).attr("data-state", "still");
+  }
+}
+//ajax
+function topicGiphy() {
+  $("#topicGifs").empty();
+  //variable storing data aname from the button clicked on
+  var topic = $(this).attr("data-name");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    topic + "&api_key=" + key + "&limit=10";
+  //ajax method
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    //then - promise - run this function
+    .then(function (response) {
+      //store returned object
+      var results = response.data;
+      //for array of data
+      for (var i = 0; i < results.length; i++) {
+        //for div
+        var topicDiv = $("<div>");
+        //add a giff class
+        topicDiv.addClass("gifs card bg-transparent border-light float-left");
+        //var rating = results[i].rating;
+        //store a p element -- rating
+        var p = $('<p class = "blockquote-footer">').text("Rating: " + results[i].rating);
+        //for the images - element
+        var topicImage = $("<img>");
+        //still
+        topicImage.attr("src", results[i].images.fixed_height_still.url);
+        //animate
+        topicImage.attr("data-animate", results[i].images.fixed_height.url);
+        //still
+        topicImage.attr("data-still", results[i].images.fixed_height_still.url);
+        //image state?
+        topicImage.attr("data-state", "still");
+        topicImage.addClass("gif img-responsive img-thumbnail rounded float-left img-fluid");
+        //addition image info - alt
+        topicImage.attr("alt", results[i].title);
+        topicDiv.append(topicImage);
+        topicDiv.append(p);
+        //add idv to the topic gif area
+        $("#topicGifs").prepend(topicDiv)
+      }
+    });
+}
+//buttons - creating the buttons
+function createButton() {
+  $("#buttons-view").empty();
+  //array of topics
+  for (var i = 0; i < topics.length; i++) {
+    //for button
+    var j = $("<button>");
+    //for button - adding class
+    j.addClass("topic-btn btn btn-info");
+    j.attr("type", "button");
+    //adding name to button
+    j.attr("data-name", topics[i]);
+    j.text(topics[i]);
+    $("#buttons-view").append(j);
+  }
+}
+//on click or event listener??
+$("#add-topic").on("click", function (event) {
+    //phil showed this -- prevent form from refreshing automatically
+    event.preventDefault();
+    //adding the new topic
+    var newTopic = $("#topic-input")
+        .val()
+        .trim();
+    if (newTopic === undefined || newTopic.length == 0) {
+        return;
+    } else {
+        topics.push(newTopic);
+        createButton();
     }
-)}
+});
+//click on//event listener?? to topic buttons
+$(document).on("click", ".topic-btn", topicGiphy);
+//animate and still
+$(document).on("click", ".gif", animateThis);
+createButton();
+
+
